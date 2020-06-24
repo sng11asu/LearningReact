d607139a91f7d4bc5f83981562c1a1514a8925cd
Ashutosh Singh <sng11asu@gmail.com>
2020-06-24 19:57:55 +0530

    react

---
+++ learnreact/build/precache-manifest.669cf0870b544d5e4ec70c4f68b714f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8151db5466118063814f19f5edcca96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "655b08a7cbb31020b31f",
+    "url": "/static/css/main.b93f424a.chunk.css"
+  },
+  {
+    "revision": "ab0c22499b0c36283b97",
+    "url": "/static/js/2.6370b103.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6370b103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655b08a7cbb31020b31f",
+    "url": "/static/js/main.96f97a7e.chunk.js"
+  },
+  {
+    "revision": "c9eb9d57452d3305d40a",
+    "url": "/static/js/runtime-main.140fe0d5.js"
+  }
+]);
